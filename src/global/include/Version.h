20f883e406fefcd7064929499fac752a2a1fe61d
MYSTICBOY <MYSTICBOY@d3ca684f-e0f0-5b4f-a4f2-b04539e05353>
2012-10-10 08:23:26 +0000

    git-svn-id: svn://192.168.60.200/JKZG/branches/ic_src/KT@2911 d3ca684f-e0f0-5b4f-a4f2-b04539e05353

--- src/global/include/Version.h
+++ src/global/include/Version.h
@@ -2,7 +2,7 @@
 #define __VersionL_H__
 #define GREATEVER "V4"
 #define LASTVER "01"
-#define BUILDDATA "1210101126"
+#define BUILDDATA "1210101517"
 #define INDEXVER  "0001"
 #define PROVER GREATEVER"."LASTVER"."BUILDDATA"."INDEXVER
 
