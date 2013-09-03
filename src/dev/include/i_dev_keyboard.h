6d768db1d9898aa301631d4f6f5e422a6dd6bcf4
mym <mym@d3ca684f-e0f0-5b4f-a4f2-b04539e05353>
2013-09-03 08:23:05 +0000

    修改键码定义
    git-svn-id: svn://192.168.60.200/JKZG/branches/ic_src/KT@4053 d3ca684f-e0f0-5b4f-a4f2-b04539e05353

--- src/dev/include/i_dev_keyboard.h
+++ src/dev/include/i_dev_keyboard.h
@@ -55,63 +55,54 @@
 	      
 #endif
 #ifdef G30KEYBORD
-	#define VK_System 18 
-	#define VK_CANCEL 1 
-	#define VK_URGENT 23 
-	#define VK_XIABAN 46 
-	#define VK_SHANGBAN 48 
-	#define VK_CLOSE_YPD 45 
-	#define VK_OPEN_YPD 44 
-	#define VK_QIANKUAN 34 
-	#define VK_BEIYONG 19 
-	#define VK_UP 31 
-	#define VK_DOWN 17 
-	#define VK_WEIZHANG 36 
-	#define VK_JUNCHE 37 
-	#define VK_GONGWU 38 
-	#define VK_Dot 52 
-	#define VK_0 11 
-	#define VK_00 0 
-	#define VK_1 2 
-	#define VK_2 3 
-	#define VK_3 4 
-	#define VK_4 5 
-	#define VK_5 6 
-	#define VK_6 7 
-	#define VK_7 8 
-	#define VK_8 9 
-	#define VK_9 10 
-	#define VK_YOUHUICARD 24 
-	#define VK_MOUNTHCARD 25 
-	#define VK_LVSETONGDAO 47 
-	#define VK_LONGYONG 49 
-	#define VK_SVCCARD 20 
-	#define VK_TYYLCARD 50 
-	#define VK_Weight 16 
-	#define VK_IC 22 
-	#define VK_WeightDaoche 33 
-	#define VK_WeightShouwei 21 
-	#define VK_MONI 35 
-	#define VK_RETURN 28 
-	#define VK_TRUCK 13 
-	#define VK_JIAOJIE 251 
-	#define VK_QIANKUANCHE 252 
-	#define VK_CHANGE 253 
-	#define VK_CHEDUI_G 254 
-	#define VK_RIGHT 255 
-	#define VK_LEFT 256 
-	#define VK_CLOSE 257 
-	#define VK_OPEN 258 
-	#define VK_CHAHAO 259
-
-
-#define  VK_CardBox 45
-#define  VK_HappyDay 20
-#define  VK_TranCard 44
-#define VK_SVC 24
-#define	VK_JZK 25
-#define VK_BEIYONG2 16
-#define 	VK_BEIYONG3 19
+	#define VK_System 300 
+	#define VK_CANCEL 301
+	#define VK_URGENT 302
+	#define VK_XIABAN 303
+	#define VK_SHANGBAN 304 
+	#define VK_CLOSE_YPD 305 
+	#define VK_OPEN_YPD 305
+	#define VK_QIANKUAN 307 
+	#define VK_BEIYONG 308
+	#define VK_UP 310 
+	#define VK_DOWN 311 
+	#define VK_WEIZHANG 312 
+	#define VK_JUNCHE 313
+	#define VK_GONGWU 314
+	#define VK_Dot 315 
+	#define VK_0 316 
+	#define VK_00 317 
+	#define VK_1 318 
+	#define VK_2 319 
+	#define VK_3 320 
+	#define VK_4 321 
+	#define VK_5 322 
+	#define VK_6 323
+	#define VK_7 324 
+	#define VK_8 325 
+	#define VK_9 326 
+	#define VK_YOUHUICARD 327 
+	#define VK_MOUNTHCARD 328 
+	#define VK_LVSETONGDAO 329 
+	#define VK_LONGYONG 330 
+	#define VK_SVCCARD 331 
+	#define VK_TYYLCARD 332 
+	#define VK_Weight 334 
+	#define VK_IC 335
+	#define VK_WeightDaoche 336 
+	#define VK_WeightShouwei 337 
+	#define VK_MONI 338
+	#define VK_RETURN 339 
+	#define VK_TRUCK 340
+	#define VK_JIAOJIE 341 
+	#define VK_QIANKUANCHE 342 
+	#define VK_CHANGE 343 
+	#define VK_CHEDUI_G 345
+	#define VK_RIGHT 346
+	#define VK_LEFT 347 
+	#define VK_CLOSE 348 
+	#define VK_OPEN 349 
+	#define VK_CHAHAO 350
 	#define VK_BREAKAUTOMAN			260
 	
 #endif
