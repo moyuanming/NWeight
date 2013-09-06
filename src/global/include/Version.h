c16d1cf9413fb07828470bfd87fef58d17c9b7eb
mym <mym@d3ca684f-e0f0-5b4f-a4f2-b04539e05353>
2013-09-06 05:46:43 +0000

    新版本号 和版本号生成工具
    git-svn-id: svn://192.168.60.200/JKZG/branches/ic_src/KT@4090 d3ca684f-e0f0-5b4f-a4f2-b04539e05353

--- src/global/include/Version.h
+++ src/global/include/Version.h
@@ -2,7 +2,7 @@
 #define __VersionL_H__
 #define GREATEVER "V4"
 #define LASTVER "01"
-#define BUILDDATA "1308021800"
+#define BUILDDATA "1309061157"
 #define INDEXVER  "4038"
 #define PROVER GREATEVER"."LASTVER"."BUILDDATA"."INDEXVER
 
